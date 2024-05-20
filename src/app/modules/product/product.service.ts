6dd776ab41eddee2ce326b8e96e5d1eb489277fa
shakib2123 <mshakibalhasan21@gmail.com>
2024-05-21 00:06:39 +0600

    service created for allProducts

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -5,7 +5,12 @@ const createProductIntoDB = async (payload: TProduct) => {
   const result = await Product.create(payload);
   return result;
 };
+const getAllProductFromDB = async () => {
+  const result = await Product.find();
+  return result;
+};
 
 export const ProductServices = {
   createProductIntoDB,
+  getAllProductFromDB,
 };
