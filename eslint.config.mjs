ec333f95d0d191c93f6de5f5626014dda4744008
shakib2123 <mshakibalhasan21@gmail.com>
2024-05-22 17:56:18 +0600

    project modified

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,26 @@
-import eslint from '@eslint/js';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommended,
+export default [
   {
+    ignores: ["**/node_modules/", ".dist/"],
     languageOptions: {
       globals: {
-        ...globals.node,
+        ...globals.browser,
+        process: "readonly",
       },
     },
-  },
-  {
+
     rules: {
-      'no-unused-vars': 'error',
-      'no-undef': 'error',
-      'prefer-const': 'error',
-      'no-console': 'warn',
+      "no-unused-vars": "error",
+      "no-unused-expressions": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
+      "no-undef": "error",
     },
   },
-  {
-    ignores: ['**/node_modules/', '**/dist/'],
-  },
-);
+
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
